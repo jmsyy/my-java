f57d47d786acc96dc198f42519e94c9c4d44ebbf
于洋19 <10038329@GJCKGD03882deMacBook-Pro.local>
2023-01-07 19:41:25 +0800

    Java demo

---
+++ GameLang.java
@@ -0,0 +1,8 @@
+
+public class GameLang {
+    public static void main(String[] args) {
+        Player P1 = new Player();
+        P1.sayName();
+        System.out.print("Hello Java");
+    }
+}
---
+++ Player.java
@@ -0,0 +1,7 @@
+public class Player {
+    int age = 5;
+    String name = "yuyang";
+    public void sayName() {
+        System.out.println(name);
+    }
+}
