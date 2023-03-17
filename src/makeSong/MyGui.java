fcfd7c12a2ce4a5e52f31e65d4f1591594572a2f
于洋19 <10038329@GJCKGD03882deMacBook-Pro.local>
2023-03-17 18:12:39 +0800

    upd: JFrame

--- src/makeSong/MyGui.java
+++ src/makeSong/MyGui.java
@@ -1,10 +1,14 @@
 package src.makeSong;
 import javax.swing.*;
+import java.awt.event.*;
 
-public class MyGui {
-  public static void main(String[] args) {
+public class MyGui implements ActionListener{
+  JButton button;
+  public void start() {
     JFrame frame = new JFrame();
-    JButton button =  new JButton(" click me ");
+    button =  new JButton(" click me ");
+
+    button.addActionListener(this);
 
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -13,5 +17,13 @@ public class MyGui {
     frame.setSize(300, 300);
 
     frame.setVisible(true);
+  }
+
+  public void actionPerformed (ActionEvent event ) {
+    button.setText(" haved change ");
+  }
+  public static void main(String[] args) {
+    MyGui gui = new MyGui();
+    gui.start();
   }
 }
